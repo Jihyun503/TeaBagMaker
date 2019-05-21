78ac36363a3718f051ffaf74f67f8a58763718ad
Jihyun Lee <s2017s17@e-mirim.hs.kr>
2019-05-21 10:53:45 +0900

    #1 Form Design 완료

--- TeaBagMaker/Form1.cs
+++ TeaBagMaker/Form1.cs
@@ -12,9 +12,38 @@ namespace TeaBagMaker
 {
     public partial class Form1 : Form
     {
+        string[] teaList = new string[] { "홍차", "녹차", "루이보스차", "국화차" };
+        string orgStr = "";
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for(int i=0; i<teaList.Length; i++)
+            {
+                this.comboBox1.Items.Add(teaList[i]);
+            }
+            if (comboBox1.Items.Count > 0)
+            {
+                this.comboBox1.SelectedIndex = 0;
+            }
+            
+            
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (this.comboBox1.SelectedIndex == 0)
+            {
+
+            }
         }
     }
 }
